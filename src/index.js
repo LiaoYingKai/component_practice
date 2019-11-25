8b60ad3725d4fa269d25bc227eec69637403d0e2
kai <b10223052@yuntech.org.tw>
2019-11-25 17:08:54 +0800

    init file

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Plugin from './plugin';
+
+function App() {
+	return (
+		<Plugin/>
+	);
+}
+
+ReactDOM.render(<App/>, document.getElementById('App'));
---
+++ src/plugin/index.js
@@ -0,0 +1,16 @@
+import React, { useState, } from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {};
+
+function DatePicker() {
+	return (
+		<div>
+			<input></input>
+		</div>
+	);
+}
+
+DatePicker.propTypes = propTypes;
+
+export default DatePicker;
