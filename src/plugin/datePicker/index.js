164455dd5b41f18ec0290d4cf4c56673930fc415
kai <b10223052@yuntech.org.tw>
2019-11-26 13:29:03 +0800

    add calendar mode and change mode function

--- src/plugin/datePicker/index.js
+++ src/plugin/datePicker/index.js
@@ -8,7 +8,17 @@ const PREFIX_CLASS = 'date-picker';
 const propTypes = {
 	className: PropTypes.string,
 };
-const MONTH = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec',];
+const MonthEnums = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec',];
+const CALENDAR_MODE = {
+	DATE: 'date',
+	MONTH: 'month',
+	YEAR: 'year',
+};
+const {
+	DATE,
+	MONTH,
+	YEAR,
+} = CALENDAR_MODE;
 
 function DatePicker({
 	className,
@@ -17,9 +27,45 @@ function DatePicker({
 	const [year, setYear] = useState(today.getFullYear());
 	const [month, setMonth] = useState(today.getMonth());
 	const [date, setDate] = useState(today.getDate());
-
 	const [calendarVisible, setCalendarVisable] = useState(true);
-	
+	const [mode, setMode] = useState(DATE);
+	const calendarMode = {
+		[DATE]: {
+			optionsClick: changeMonth,
+			header: `${MonthEnums[month]} ${year}`,
+			body: '',
+		},
+		[MONTH]: {
+			optionsClick: changeYear,
+			header: `${year}`,
+			body: '',
+		},
+		[YEAR]: {
+			optionsClick: () => {},
+			header: `year Range`,
+			body: '',
+		},
+	};
+
+	function changeMode() {
+		switch (mode) {
+			case DATE: {
+				setMode(MONTH);
+				break;
+			}
+			case MONTH: {
+				setMode(YEAR);
+				break;
+			}
+			case YEAR: {
+				setMode(DATE);
+				break;
+			}
+			default: {
+				setMode(DATE);
+			}
+		}
+	}
 	function changeMonth(event) {
 		if (month + event === 12) {
 			setYear(year + 1);
@@ -31,7 +77,12 @@ function DatePicker({
 			setMonth(month + event);
 		}
 	}
+	function changeYear(event) {
+		setYear(year + event);
+	}
+	function changeYearRange(event) {
 
+	}
 	return (
 		<div className={cx(PREFIX_CLASS, className)}>
 			<input 
@@ -44,11 +95,11 @@ function DatePicker({
 				{ [`${PREFIX_CLASS}__calendar--visible`]: calendarVisible }
 			)}>
 				<div className={`${PREFIX_CLASS}__calendar-header`}>
-					<span onClick={() => {changeMonth(-1);}}> &lt; </span>
-					<div>
-						{MONTH[month]} {year}
+					<span onClick={() => {calendarMode[mode].optionsClick(-1);}}> &lt; </span>
+					<div onClick={changeMode}>
+						{calendarMode[mode].header}
 					</div>
-					<span onClick={() => {changeMonth(1);}}> &gt; </span>
+					<span onClick={() => {calendarMode[mode].optionsClick(1);}}> &gt; </span>
 				</div>
 				<div className={`${PREFIX_CLASS}__calendar-body`}>改變的區塊</div>
 			</div>
